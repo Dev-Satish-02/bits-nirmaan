50214a8f455adcb5d11b788a4493f0d783223770
dev <devsatish08@gmail.com>
2024-11-16 23:57:37 +0530

    scroller

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProjectsComponent } from '../projects/projects.component';
 import { EventsComponent } from '../events/events.component';
 import { HeroComponent } from '../hero/hero.component';
+import { ScrollerComponent } from "../scroller/scroller.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, ProjectsComponent, EventsComponent],
+  imports: [HeroComponent, ProjectsComponent, EventsComponent, ScrollerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
