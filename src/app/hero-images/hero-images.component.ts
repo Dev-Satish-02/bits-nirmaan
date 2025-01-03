b9ce3b09b370117e0bf083c717ff8f2042f377e4
dev <devsatish08@gmail.com>
2025-01-03 18:22:26 +0530

    images

--- src/app/hero-images/hero-images.component.ts
+++ src/app/hero-images/hero-images.component.ts
@@ -3,27 +3,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero-images',
-  template: `
-    <div style="text-align: center; position: relative;">
-      <button (click)="nextImage()" style="background: none; border: none; padding: 0; cursor: pointer;">
-        <img [src]="images[currentImageIndex]" alt="Slider Image" style="max-width: 100%; height: auto;">
-      </button>
-    </div>
-  `,
+  templateUrl: './hero-images.component.html',
   styleUrls: ['./hero-images.component.css'],
   imports: [CommonModule],
   standalone: true
 })
 export class HeroImagesComponent {
   images: string[] = [
-    'https://raw.githubusercontent.com/Dev-Satish-01/images/refs/heads/main/bits-nirmaan/hero1.jpeg',
-    'https://raw.githubusercontent.com/Dev-Satish-01/images/refs/heads/main/bits-nirmaan/hero2.jpeg',
-    'https://raw.githubusercontent.com/Dev-Satish-01/images/refs/heads/main/bits-nirmaan/hero3.jpeg'
+    'images/hero/1.jpeg',
+    'images/hero/2.jpeg',
+    'images/hero/3.jpeg'
   ];
 
   currentImageIndex: number = 0;
 
   nextImage() {
-    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length; // Change to the next image
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
   }
 }
